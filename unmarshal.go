4b86f7020ec76baf67b73f40dfdfb6739524d71d
Victor Schappert <schapper@amazon.com>
2021-07-09 17:50:36 -0700

    Start sketching out struct unmarshaling

--- unmarshal.go
+++ unmarshal.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"reflect"
 	"strconv"
+	"strings"
+	"time"
 )
 
 // Unmarshal converts the CloudWatch Logs Insights result data and stores the
@@ -75,7 +77,16 @@ func array(data []Result, a reflect.Value, rv reflect.Value) error {
 
 type resultUnpackFunc func(i int, r Result, v reflect.Value) error
 
-type fieldUnpackFunc func(i, j int, field, value string) (reflect.Value, error)
+type resultFieldUnpackFunc func(i, j int, field, value string) (reflect.Value, error)
+
+type structFieldUnpackFunc func(src string, dst reflect.Value) error
+
+type structFieldUnpackFuncSelector func(t reflect.Type) (structFieldUnpackFunc, error)
+
+type unpackableStructField struct {
+	fieldIndex int
+	unpackFunc structFieldUnpackFunc
+}
 
 var (
 	resultType = reflect.TypeOf(Result{})
@@ -105,6 +116,7 @@ func selectResultUnpackFuncPtr(ptrType reflect.Type, rv reflect.Value) (resultUn
 	if err != nil {
 		return nil, err
 	}
+	// FIXME: Remove this comment. This pointerization chain IS consistent with how encoding/json works: https://play.golang.org/p/a-uRZjmuqTJ
 	return func(i int, r Result, ptr reflect.Value) error {
 		elem := reflect.New(elemType)
 		err := f(i, r, elem)
@@ -131,6 +143,8 @@ func selectResultUnpackFuncMap(mapType reflect.Type, rv reflect.Value) (resultUn
 		return unpackMapStringUint(valueType), nil
 	case reflect.Float32, reflect.Float64:
 		return unpackMapStringFloat(valueType), nil
+	case reflect.Bool:
+		return unpackMapStringBool(valueType), nil
 	case reflect.Interface:
 		return unpackMapStringJSONFuzzy, nil
 	case reflect.Map, reflect.Slice, reflect.Array:
@@ -142,37 +156,130 @@ func selectResultUnpackFuncMap(mapType reflect.Type, rv reflect.Value) (resultUn
 	}
 }
 
-func selectResultUnpackFuncStruct(structType reflect.Type, rv reflect.Value) (resultUnpackFunc, error) {
-	type structField struct {
-		fieldIndex int
-		unpackFunc fieldUnpackFunc
+func selectUnpackStructFieldInsightsTime(t reflect.Type) (structFieldUnpackFunc, error) {
+	switch t.Kind() {
+	case reflect.String:
+		return unpackStructFieldString, nil
+	case reflect.Struct:
+		if t == reflect.TypeOf(time.Time{}) {
+			return unpackStructFieldInsightsTime, nil
+		}
 	}
+	return nil, &UnmarshalResultTypeError{
+		// TODO: Properly implement this.
+	}
+}
+
+func selectUnpackStructFieldByType(t reflect.Type) (structFieldUnpackFunc, error) {
+	// FIXME: This is where I left off working on 7/9/2021.
+	//
+	// Basically the rules are:
+	//    t is a String -> put it into a string
+	//    t is a supported number type -> put it into the number.
+	//    t is a bool -> put it into the bool.
+	//    t is anything that if you take a pointer to it, it implements TextUnmarshaler -> TextUnmarshal it.
+	//    t is anything that if you take a pointer to it, it implements JSONUnmarshaler -> JSONUnmarshal it.
+	//    t is a Struct, Slice, Array, or Map -> JSON decode into the value.
+	//    else POWWWWW!
+	return nil, nil
+}
+
+func selectStructFieldUnpackFunc(selector structFieldUnpackFuncSelector, t reflect.Type) (structFieldUnpackFunc, error) {
+	if t.Kind() != reflect.Ptr {
+		return selector(t)
+	}
+	elemType := t.Elem()
+	unpacker, err := selectStructFieldUnpackFunc(selector, elemType)
+	if err != nil {
+		return nil, err
+	}
+	return func(src string, dst reflect.Value) error {
+		elem := reflect.New(elemType)
+		err := unpacker(src, elem)
+		if err != nil {
+			return err
+		}
+		dst.Set(elem)
+		return nil
+	}, nil
+}
+
+func selectResultFieldUnpackFunc(sf *reflect.StructField) (field string, unpackFunc structFieldUnpackFunc, err error) {
+	tag := sf.Tag.Get("incite")
+	if tag != "" {
+		var selector structFieldUnpackFuncSelector
+		field = tag
+		switch field {
+		case "@timestamp", "@ingestionTime":
+			selector = selectUnpackStructFieldInsightsTime
+		default:
+			selector = selectUnpackStructFieldByType
+		}
+		unpackFunc, err = selectStructFieldUnpackFunc(selector, sf.Type)
+		return
+	}
+	tag = sf.Tag.Get("json")
+	switch tag {
+	default:
+		comma := strings.IndexByte(tag, ',')
+		if comma < 0 {
+			comma = len(tag)
+		}
+		field = tag[:comma]
+		unpackFunc = unpackStructFieldJSON
+		return
+	case "-":
+		field = ""
+		unpackFunc = nil
+		return
+	case "":
+		break
+	}
+	field = sf.Name
+	unpackFunc, err = selectStructFieldUnpackFunc(selectUnpackStructFieldByType, sf.Type)
+	return
+}
+
+func selectResultUnpackFuncStruct(structType reflect.Type, rv reflect.Value) (resultUnpackFunc, error) {
 	// I want to traverse the struct look at the fields and tags:
 	//    - Throw an error if any invalid types exist, for example try to put @message into int or it's a channel or something,
 	//      or try to put @ingestionTime or @timestamp into a field that's not a string, big enough integer, or time.Time{}.
-	//    - Make a map[string]fieldUnpackFunc for all discovered fields in the
+	//      Note @timestamp format is "2021-06-19 03:59:59.936" (in UTC) with a space before the timestamp and now time zone.
+	//      This is an example of the format: https://play.golang.org/p/USdNBPM-mVv. Note that time.UnmarshalText won't
+	//      unmarshal it, will return error `parsing time "2021-06-19 03:59:59.936" as "2006-01-02T15:04:05Z07:00": cannot parse " 03:59:59.936" as "T"`
+	//    - Make a map[string]resultFieldUnpackFunc for all discovered fields in the
 	//      struct. That way when we walk through the ResultFields in each Result, it's super trivial
 	//      to just lookup the single thing that does the thing.
+	//
+	// Demo of how tags work: https://play.golang.org/p/tt8t4zN8KWO
 	n := structType.NumField()
-	sfs := make(map[string]structField, n)
+	ufs := make(map[string]unpackableStructField, n)
 	for i := 0; i < n; i++ {
 		if false {
-			field := "foo"
-			sfs[field] = structField{
-				fieldIndex: i,
-				// TODO need to figure out the unpack func.
+			sf := structType.Field(i)
+			field, unpackFunc, err := selectResultFieldUnpackFunc(&sf)
+			if err != nil {
+				return nil, err // TODO: Return the correct InvalidUnmarshal type error.
+			}
+			if unpackFunc != nil {
+				ufs[field] = unpackableStructField{
+					fieldIndex: i,
+					unpackFunc: unpackFunc,
+					// TODO need to figure out the unpack func.
+				}
 			}
 		}
 	}
 	return func(i int, r Result, v reflect.Value) error {
 		for j := range r.Fields {
 			field := r.Fields[j].Field
-			if sf, ok := sfs[field]; ok {
-				value, err := sf.unpackFunc(i, j, field, r.Fields[j].Value)
+			if uf, ok := ufs[field]; ok {
+				src := r.Fields[j].Value
+				dst := v.Field(uf.fieldIndex)
+				err := uf.unpackFunc(src, dst)
 				if err != nil {
 					return err // TODO: return the correct error
 				}
-				v.Field(sf.fieldIndex).Set(value)
 			}
 		}
 		return nil
@@ -184,7 +291,7 @@ func unpackResultCopy(_ int, r Result, v reflect.Value) error {
 	return nil
 }
 
-func unpackMapStringAnyElem(i int, r Result, mapValue reflect.Value, conv fieldUnpackFunc) error {
+func unpackMapStringAnyElem(i int, r Result, mapValue reflect.Value, conv resultFieldUnpackFunc) error {
 	if mapValue.IsNil() {
 		mapValue.Set(reflect.MakeMap(mapValue.Type()))
 	}
@@ -274,7 +381,7 @@ func unpackMapStringJSONFuzzy(i int, r Result, mapValue reflect.Value) error {
 			case '\t', '\n', '\r', ' ': // Might be JSON, keep skipping whitespace to find out.
 				break
 			case '{', '[', '"': // Might be JSON, try to unpack it.
-				v, err := unpackJSONFuzzy(value)
+				v, err := unpackStringJSONFuzzy(value)
 				if err != nil {
 					return v, nil
 				}
@@ -299,13 +406,35 @@ func unpackMapStringJSONAggregate(i int, r Result, mapValue reflect.Value) error
 	})
 }
 
-func unpackJSONFuzzy(s string) (reflect.Value, error) {
+func unpackStringJSONFuzzy(s string) (reflect.Value, error) {
 	var i interface{}
 	err := json.Unmarshal([]byte(s), &i)
 	if err != nil {
 		return reflect.Value{}, err
 	}
 	return reflect.ValueOf(i), nil
+}
+
+func unpackStructFieldJSON(src string, dst reflect.Value) error {
+	ptr := dst.Addr()
+	i := ptr.Interface()
+	return json.Unmarshal([]byte(src), i)
+}
+
+func unpackStructFieldString(src string, dst reflect.Value) error {
+	dst.Set(reflect.ValueOf(src))
+	return nil
+}
+
+func unpackStructFieldInsightsTime(src string, dst reflect.Value) error {
+	t, err := time.Parse("", src)
+	if err != nil {
+		return &UnmarshalResultTypeError{
+			// TODO properly implement this
+		}
+	}
+	dst.Set(reflect.ValueOf(t))
+	return nil
 }
 
 func resultCopy(r Result) Result {
@@ -387,9 +516,4 @@ type UnmarshalResultJSONSyntaxError struct {
 
 func (e *UnmarshalResultJSONSyntaxError) Error() string {
 	return "TODO: pls make me work properly"
-}
-
-type UnmarshalJSONError struct {
-	Index  int
-	Result Result
 }
