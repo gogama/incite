c81961f21306b82d483a61b93baee365f9e190b5
Victor Schappert <schapper@amazon.com>
2021-07-13 16:29:47 -0700

    Export constant value for @timestamp layout

---
+++ time.go
@@ -0,0 +1,13 @@
+package incite
+
+// TimeLayout is a Go time layout which defines the format of the time values
+// returned in the timestamp fields of CloudWatch Logs Insights queries.
+//
+// TimeLayout defines the format by showing how the Go reference time of
+//
+// 	Mon Jan 2 15:04:05 -0700 MST 2006
+//
+// would be formatted if it were the value. TimeLayout can be used with
+// time.Parse to parse timestamp fields, such as @timestamp and @ingestionTime,
+// which are returned in CloudWatch Logs Insights query results.
+const TimeLayout = "2006-01-02 15:04:05.000"
