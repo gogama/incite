471fe402a3db9ca8fd7ae8bae6a66e1b54f7a6e6
Victor Schappert <victor.schappert@protonmail.com>
2021-07-15 23:36:02 -0700

    Start adding testable examples for the unmarshaler

--- example_test.go
+++ example_test.go
@@ -1,5 +1,55 @@
-package incite
+package incite_test
 
-// TODO: Test examples.
+import (
+	"fmt"
+
+	"github.com/gogama/incite"
+)
+
+// TODO: More examples: Unmarshal struct with/without tags.
+//                    : Timestamps.
+//                    : Copy.
 //
 // https://blog.golang.org/examples
+
+func ExampleUnmarshal_mapStringString() {
+	data := []incite.Result{
+		[]incite.ResultField{{Field: "@ptr", Value: "foo"}, {Field: "@message", Value: "bar"}},
+	}
+	var v []map[string]string
+	_ = incite.Unmarshal(data, &v) // Error ignored for simplicity.
+	fmt.Println(v)
+	// Output: [map[@message:bar @ptr:foo]]
+}
+
+func ExampleUnmarshal_interface() {
+	// An interface{} is treated as []map[string]string. The Object
+	// key's value is not deserialized from JSON, it remains a string.
+	data := []incite.Result{
+		[]incite.ResultField{{Field: "@ptr", Value: `abc123`}, {Field: "Object", Value: `{"key":"value"}`}},
+	}
+	var v interface{}
+	_ = incite.Unmarshal(data, &v) // Error ignored for simplicity.
+	fmt.Println(v)
+	// Output: [map[@ptr:abc123 Object:{"key":"value"}]]
+}
+
+func ExampleUnmarshal_mapStringInterface() {
+	// As a special case, the data are unmarshalled fuzzily if the target
+	// is a map[string]interface{}. If a value is valid JSON it is
+	// unmarshalled as JSON, otherwise it is kept as a string. Here the
+	// Object and QuotedString fields are contain valid JSON so they
+	// unmarshal as a map and string, respectively. UnquotedString is
+	// not valid JSON and stays as a string.
+	data := []incite.Result{
+		[]incite.ResultField{
+			{Field: "Object", Value: `{"key":"value"}`},
+			{Field: "QuotedString", Value: `"hello"`},
+			{Field: "UnquotedString", Value: `world`},
+		},
+	}
+	var v []map[string]interface{}
+	_ = incite.Unmarshal(data, &v) // Error ignored for simplicity.
+	fmt.Println(v)
+	// Output: [map[@ptr:def456 Object:map[key:value] QuotedString:hello UnquotedString:world]]
+}
