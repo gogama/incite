b23033f118f373925127479a773c3bbd6c0af3f0
Victor Schappert <victor.schappert@protonmail.com>
2022-01-30 21:14:15 -0800

    Update copyright date (very minor)

--- log_test.go
+++ log_test.go
@@ -1,4 +1,4 @@
-// Copyright 2021 The incite Authors. All rights reserved.
+// Copyright 2022 The incite Authors. All rights reserved.
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 
